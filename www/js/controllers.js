90d21b0ebfb1d138603dc9a15496e5d5456aa1c4
Budd, Samuel <Samuel.Budd@zuhlke.com>
2015-09-22 16:23:54 +0100

    now sending to data to api

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,7 +28,7 @@ angular.module('starter.controllers', [])
   .controller('LaterRoomBookingController', function ($scope, $stateParams) {
 
   })
-  .controller('MyLocationController', function ($scope, $stateParams, $rootScope, $ionicPlatform, $cordovaBeacon) {
+  .controller('MyLocationController', function ($scope, $stateParams, $rootScope, $ionicPlatform, $cordovaBeacon, $http) {
 
     $scope.beacons = {};
  
@@ -38,15 +38,39 @@ angular.module('starter.controllers', [])
  
         $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function(event, pluginResult) {
             var uniqueBeaconKey;
+            var data = [];
             for(var i = 0; i < pluginResult.beacons.length; i++) {
-              //send data here
+              if (pluginResult.beacons[i].major == 14470) {
+                data.push({
+                  "sensorId": "FUTURE-3",
+                  "distance": pluginResult.beacons[i].rssi,
+                });
+              }
+              else if (pluginResult.beacons[i].major == 1000) {
+                data.push({
+                  "sensorId": "FUTURE-2",
+                  "distance": pluginResult.beacons[i].rssi,
+                });
+              } else {
+                data.push({
+                  "sensorId": "FUTURE-1",
+                  "distance": pluginResult.beacons[i].rssi,
+                });
+              }
                 uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
-                $scope.beacons[i] = pluginResult.beacons[i];
+                $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
+                $http.put('http://10.132.32.162:3030/phones/sam/beaconData', data)
+                .then(function(response) {
+                    console.log(response);
+                })
+                .catch(function(response) {
+                    console.log(response);
+                });
 
             }
             $scope.$apply();
         });
- 
+
         $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("Light Blue", "b9407f30-f5f8-466e-aff9-25556b57fe6d", 14470, 61580));
         $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("Purple", "b9407f30-f5f8-466e-aff9-25556b57fe6d", 1000, 1));
         $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("Green", "b9407f30-f5f8-466e-aff9-25556b57fe6d", 4473, 1));
